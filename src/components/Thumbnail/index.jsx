2474737951605226fa9e40080389f975092335c8
AlahmadiQ8 <m91.alahmadi@gmail.com>
2017-11-27 02:19:01 -0800

    responsive first css, newer design of nav and banner

--- src/components/Thumbnail/index.jsx
+++ src/components/Thumbnail/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import './index.scss';
-import IconsContainer from './IconsContainer';
 import thumbnail from './thumbnail.jpg';
 
 const Thumbnail = () => (
   <div className="thumbnail">
     <img className="thumbnail__img" src={thumbnail} alt="Mohammad Alahmadi" />
-    {/* <IconsContainer /> */}
   </div>
 );
 
