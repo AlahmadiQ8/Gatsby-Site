df2bd52119ad605eb0ed7362d0865943c28e77a4
AlahmadiQ8 <m91.alahmadi@gmail.com>
2017-12-05 23:52:40 -0800

    index page - remove shadow and use borders

--- src/components/PageIndex/ProjectEntry.jsx
+++ src/components/PageIndex/ProjectEntry.jsx
@@ -6,25 +6,24 @@ import Flex from '../Flex';
 
 const ProjectEntry = ({ title, desc, imgURl, alt, small }) => {
   const img = <img className="entry__img" src={imgURl} alt={alt} />;
-  const className = cx(
-    'entry',
-    { 'entry--border-bottom': !imgURl }
-  );
+  const className = cx('entry');
   const h2ClassName = cx(
     'entry__h2',
     { 'entry__h2--small': small }
   );
 
   return (
-    <div className={className}>
-      <Flex valign="center">
-        {imgURl ? img : null}
-        <div>
-          <Link to="#"><h2 className={h2ClassName}>{title}</h2></Link>
-          <p className="entry__p">{desc}</p>
-        </div>
-      </Flex>
-    </div>
+    <Link to="#">
+      <div className={className}>
+        <Flex valign="center">
+          {imgURl ? img : null}
+          <div>
+            <Link to="#"><h2 className={h2ClassName}>{title}</h2></Link>
+            <p className="entry__p">{desc}</p>
+          </div>
+        </Flex>
+      </div>
+    </Link>
   );
 };
 
